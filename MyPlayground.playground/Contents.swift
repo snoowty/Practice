c22dc013593aece0017c33b6e6b69c5ea624044b
snoowty <tanosiienjoy@gmail.com>
2018-05-17 22:25:56 +0900

    Chapter 4

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -10,3 +10,26 @@ for n in 1...100{
     a = a + n;
 }
 print(a)
+
+//Chapter 4
+
+var bodyTemp : Double = 36
+bodyTemp = 36.5
+
+var test = [95, 70, 80]
+var sum = 0
+for num in test{
+    sum = num + sum
+}
+print(sum)
+
+var testDictionaly = ["国語":95, "数学":70,"英語":80]
+testDictionaly["数学"] = 100
+
+func areaOfSquare (side :Int) -> Int{
+    let result = side * side
+    return result
+}
+print(areaOfSquare(side: 3))
+
+
