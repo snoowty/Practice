ded5f235c0dbc197b16945a37f36106f760be29c
snoowty <tanosiienjoy@gmail.com>
2018-05-22 22:55:17 +0900

    Auto Layout

---
+++ Hello/Hello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by Takahiro Yasuda on 2018/05/22.
+//  Copyright © 2018年 Yasuda Takahiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
