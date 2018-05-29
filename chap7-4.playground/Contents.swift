0d1ef39771a8b136ff625a037dcfe80f61bc1c0b
snoowty <tanosiienjoy@gmail.com>
2018-05-29 23:32:34 +0900

    Any型とまとめ

--- chap7-4.playground/Contents.swift
+++ chap7-4.playground/Contents.swift
@@ -10,3 +10,17 @@ class IPhone {
 var iPhone = IPhone(language:"日本語", passcode:"0123")
 
 print(iPhone.language)
+
+var string:Any = "こんにちは"
+string = 10
+
+//デフォルトイニシャライザ
+class IPhone2 {
+    var language = "日本語"
+    var passcode = "0123"
+    init() {
+    }
+}
+
+//ラベル
+//省略すると自動的に引数名と同名、アンダースコアにすると呼び出し時にラベルを書けない（書くと英文が不自然になる時に使用）。
